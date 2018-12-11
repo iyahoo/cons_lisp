be01b155a463d152368d8672b9cc735ff768ad42
iyahoo <s1200191@gmail.com>
2018-12-11 04:23:36 +0000

    :sparkles: [Add] pSpace

--- pages/index.vue
+++ pages/index.vue
@@ -10,9 +10,11 @@
             rows="1"
             id="inputArea"
          />
-         <v-text-field :value="eval" label="Output" outline readonly />
+         <v-text-field :value="tryParser" label="Parsed" outline readonly />
+         <h3>Tests</h3>
          <v-layout row wrap>
             <v-flex xs12 md6 v-for="(t, i) in testings" :key="i">
+               <h4>{{ t.name }}</h4>
                <v-text-field
                   :value="JSON.stringify(t.function(t.input))"
                   :label="testToStr(t)"
@@ -35,19 +37,44 @@ export default {
       },
 
       // perser
+      pSpace({ parsed, rest }) {
+         const r = /\s+(.*)/
+         const result = rest.match(r)
+         if (!result) {
+            return { parsed: parsed, rest: rest }
+         } else {
+            return { parsed: parsed, rest: result[1] }
+         }
+      },
       pNumber({ parsed, rest }) {
          const r = /(^[1-9]+)(.*)/
          const result = rest.match(r)
          if (!result) {
             return { parsed: parsed, rest: rest }
          } else {
-            return { parsed: parseInt(result[1]), rest: result[2] }
+            if (parsed === null) {
+               return { parsed: parseInt(result[1]), rest: result[2] }
+            } else {
+               return parser({
+                  parsed: parsed.concat(parseInt(result[1])),
+                  rest: result[2],
+               })
+            }
+         }
+      },
+      pList({ parsed, rest }) {
+         const r = /^\((.+)\)/
+         const result = rest.match(r)
+         if (!result) {
+            return { parsed: parsed, rest: rest }
+         } else {
+            return { parsed: parsed, rest: rest }
          }
       },
       parser(pd) {
-         const result = this.pNumber(pd)
+         const result = this.pList(this.pNumber(this.pSpace(pd)))
          if (result.parsed === null) {
-            return false
+            throw new Error('parser error')
          } else {
             return result
          }
@@ -64,29 +91,34 @@ export default {
       testToColor(t) {
          const actual = t.function(t.input)
 
-         return this.objectEquality(actual, t.function(t.input))
-            ? 'green'
-            : 'red'
+         return this.objectEquality(actual, t.expect) ? 'green' : 'red'
       },
 
       objectEquality(a, b) {
          // only for unnested object
-         let r = true
-         for (const k in a) {
-            r &= a[k] === b[k]
+
+         if (a === b) {
+            return true
          }
-         return r
+         if (typeof a === 'object' && typeof b === 'object') {
+            let r = true
+            for (const k in a) {
+               r &= a[k] === b[k]
+            }
+            return r
+         }
+         return false
       },
    },
    computed: {
-      eval() {
-         const result = this.parser({ parsed: null, rest: this.inputstr })
-         if (!result) {
-            return 'Parser error!'
+      tryParser() {
+         try {
+            const result = this.parser({ parsed: null, rest: this.inputstr })
+            const { parsed } = result
+            return parsed
+         } catch (e) {
+            return e
          }
-
-         const { parsed } = result
-         return parsed
       },
    },
 
@@ -97,14 +129,34 @@ export default {
          // test
          testings: [
             {
+               name: 'pNumber1',
                input: { parsed: null, rest: '33' },
                function: this.pNumber,
                expect: { parsed: 33, rest: '' },
             },
             {
+               name: 'pNumber2',
                input: { parsed: null, rest: 'hoge' },
                function: this.pNumber,
                expect: { parsed: null, rest: 'hoge' },
+            },
+            {
+               name: 'pSpace1',
+               input: { parsed: null, rest: '   ' },
+               function: this.pSpace,
+               expect: { parsed: null, rest: '' },
+            },
+            {
+               name: 'pSpace2',
+               input: { parsed: null, rest: '   hoge' },
+               function: this.pSpace,
+               expect: { parsed: null, rest: 'hoge' },
+            },
+            {
+               name: 'check equality of null and empty string',
+               input: null === '',
+               function: x => x,
+               expect: false,
             },
          ],
       }
